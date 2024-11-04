fb797157121fd23d505d3c5dd087983256370624
Jan Druszcz <jan.druszcz@onet.eu>
2024-11-04 21:56:34 +0100

    Add files via upload

---
+++ lab3/src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import UI.UI;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        UI menu = new UI();
+        menu.displayMainMenu();
+    }
+}
